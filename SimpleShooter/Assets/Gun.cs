845f4bbfbaca5594c20bdea69df65203b1699836
maifeeulasad <maifeeulasad@gmail.com>
2020-01-23 20:16:11 +0600

    random rotation

--- SimpleShooter/Assets/Gun.cs
+++ SimpleShooter/Assets/Gun.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Gun : MonoBehaviour
 {
     public int speed = 10;
-    float modifier = 0.00001f;
     public GameObject bullet;
     GameObject spawnPoint;
 
@@ -22,9 +21,7 @@ public class Gun : MonoBehaviour
             bulletI.GetComponent<Rigidbody>().velocity = bulletI.transform.forward * -speed;
             Destroy(bulletI, 20);
         }
-        if (Input.GetKey(KeyCode.W))
-            transform.Rotate(transform.rotation.eulerAngles + new Vector3(0, 0, 1) * modifier, Space.Self);
-        if (Input.GetKey(KeyCode.S))
-            transform.Rotate(transform.rotation.eulerAngles - new Vector3(0, 0, 1) * modifier, Space.Self);
+        if (Input.GetKeyDown(KeyCode.X))
+            transform.rotation = new Quaternion(Random.Range(-1, 1), Random.Range(-1, 1), Random.Range(-1, 1), Random.Range(-1, 1));
     }
 }
