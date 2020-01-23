06fa9a9cefda67740ae66f2c38deb45c9e9fbed2
maifeeulasad <maifeeulasad@gmail.com>
2020-01-23 20:00:34 +0600

    just instantiate at spawn point

--- SimpleShooter/Assets/Gun.cs
+++ SimpleShooter/Assets/Gun.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class Gun : MonoBehaviour
 {
     float modifier = 0.00001f;
+    public GameObject bullet;
+    GameObject spawnPoint;
+
+    void Start()
+    {
+        spawnPoint = transform.GetChild(0).gameObject;
+    }
 
     void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bullet, spawnPoint.transform);
+        }
+        if (Input.GetKey(KeyCode.W))
             transform.Rotate(transform.rotation.eulerAngles + new Vector3(0, 0, 1) * modifier, Space.Self);
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKey(KeyCode.S))
             transform.Rotate(transform.rotation.eulerAngles - new Vector3(0, 0, 1) * modifier, Space.Self);
     }
 }
